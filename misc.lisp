09ac0ff34f7d03aa60518fa578ef9e43567ddad9
Arnold Noronha <arnold@tdrhq.com>
2022-10-29 15:28:32 -0400

    Formalize async-promoter logic
    Summary:
    ...
    
    ...
    
    ...
    
    ...
    
    ...
    
    ...
    
    ...
    
    ...
    
    ...
    
    ...
    
    Differential Revision: https://phabricator.tdrhq.com/D6006
    
    GitOrigin-RevId: 1948da3eb89f76d588e4dcf61de5ebb0c821b6dd

--- misc.lisp
+++ misc.lisp
@@ -5,9 +5,15 @@
                 #:equal-to)
   (:import-from #:fiveam-matchers/described-as
                 #:described-as)
-  (:local-nicknames (#:a #:alexandria)))
+  (:local-nicknames (#:a #:alexandria))
+  (:export
+   #:is-null))
 (in-package :fiveam-matchers/misc)
 
 (defun is-not-null ()
   (described-as "not to be null"
     (is-not (equal-to nil))))
+
+(defun is-null ()
+  (described-as "is null"
+    (equal-to nil)))
